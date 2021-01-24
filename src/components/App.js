cb43126b876e74fb315e6ec00be1e6839ff0c4ea
Fawaz Haroun <harounayomikun@gmail.com>
2021-01-24 19:58:06 +0100

    more development

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import NavBar from "./NavBar";
+import "./stylesheets/App.css"
+
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <NavBar />
+        </div>
     )
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,25 @@
 import React from "react";
 
+import "./stylesheets/NavBar.css"
+import DominosLogo from "../images/logo (1).png"
+
 const NavBar = () => {
     return (
-        <nav className="nav">NavBar</nav>
+        <nav className="nav">
+            <div className="nav-container">
+                <div className="dominos-logo-container">
+                    <img src={DominosLogo}  alt="Dominos Logo" />
+                </div>
+                <div className="main-nav-container">
+                    <ul>
+                        <li>Order</li>
+                        <li>Deals</li>
+                        <li>Location</li>
+                    </ul>
+                </div>
+                <div></div>
+            </div>
+        </nav>
     )
 }
 
