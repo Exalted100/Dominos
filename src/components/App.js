059f1eb47ab6eae476ce088c5a3144d61e74b698
Fawaz Haroun <harounayomikun@gmail.com>
2021-01-25 04:32:23 +0100

    created search bar file

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import NavBar from "./NavBar";
-import "./stylesheets/App.css"
+import SearchBar from "./SearchBar"
+import "./stylesheets/App.css";
 
 const App = () => {
     return (
