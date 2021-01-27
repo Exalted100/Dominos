8e5bde09924e495194d57822187e4ca63a6eca7b
Fawaz Haroun <harounayomikun@gmail.com>
2021-01-27 19:55:05 +0100

    Added more items to the items constant

--- src/components/Items.js
+++ src/components/Items.js
@@ -2,14 +2,14 @@ import React from "react";
 import Item from "./Item";
 import "./stylesheets/Items.css";
 
-import BarbecuePepperoni from "../images/Untitled-1 1.png";
+import BarbequePepperoni from "../images/Untitled-1 1.png";
 
 const Items = () => {
     const items = [
         {
-            name: "Barbecue Pepperoni",
+            name: "Barbeque Pepperoni",
             price: "2,700",
-            image: BarbecuePepperoni
+            image: BarbequePepperoni
         }, {
             name: "Double Cheese",
             price: "2,400",
@@ -21,6 +21,62 @@ const Items = () => {
         }, {
             name: "Supreme Chicken",
             price: "2,700",
+            image: 
+        }, {
+            name: "Chicken Suya",
+            price: "2,500",
+            image: 
+        }, {
+            name: "Pineapple Delight",
+            price: "2,500",
+            image: 
+        }, {
+            name: "Shawarma Roll",
+            price: "1,700",
+            image: 
+        }, {
+            name: "Buffalo Wings",
+            price: "1,500",
+            image: 
+        }, {
+            name: "Barbeque Wings",
+            price: "1,700",
+            image: 
+        }, {
+            name: "Coca-cola",
+            price: "300",
+            image: 
+        }, {
+            name: "Coca-cola Zero",
+            price: "300",
+            image: 
+        }, {
+            name: "Fanta",
+            price: "300",
+            image: 
+        }, {
+            name: "Sprite",
+            price: "300",
+            image: 
+        }, {
+            name: "Pepsi",
+            price: "300",
+            image: 
+        }, {
+            name: "Mirinda",
+            price: "300",
+            image: 
+        }, {
+            name: "Mountain Dew",
+            price: "300",
+            image: 
+        }, {
+            name: "Eva Water",
+            price: "300",
+            image: 
+        }, {
+            name: "Aquafina Water",
+            price: "300",
             image: 
         }
     ]
