009e5dad4f3c18958ca8cf9dbdda63d0d236799e
Fawaz Haroun <harounayomikun@gmail.com>
2021-01-27 19:38:24 +0100

    changed the Items component to dynamically render each item

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,15 @@
 import React from "react";
-import Pizza from "../images/Untitled-1 1.png";
 import "./stylesheets/Item.css";
 
-const Item = () => {
+const Item = (props) => {
     return (
         <div className="item-container">
             <div className="img-container">
-                <img src={Pizza} alt="pizza picture" />
+                <img src={props.image} alt="pizza picture" />
             </div>
-            <p className="item-name">Barbecue Pepperoni</p>
+            <p className="item-name">{props.name}</p>
             <div className="price-container">
-                <p className="item-price">From <span className="price">₦2,700</span></p>
+                <p className="item-price">From <span className="price">₦{props.price}</span></p>
                 <p className="add-to-cart">Add to Cart</p>
             </div>
         </div>
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Item from "./Item";
-import "./stylesheets/Items.css"
+import "./stylesheets/Items.css";
+
+import BarbecuePepperoni from "../images/Untitled-1 1.png";
 
 const Items = () => {
+    const items = [
+        {
+            name: "Barbecue Pepperoni",
+            price: "2,700",
+            image: BarbecuePepperoni
+        }
+    ]
+
     return (
         <div className="items-container">
-            <Item />
-            <Item />
-            <Item />
-            <Item />
+            {items.map(item => {
+                return (
+                    <div key={item.name}>
+                        <Item name={item.name} price={item.price} image={item.image} />
+                    </div>
+                )
+            })}
         </div>
     )
 }
