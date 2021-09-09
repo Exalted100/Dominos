100ba1f7bbb72da0e901ec3b4b2c3515c7677259
Fawaz Haroun <harounayomikun@gmail.com>
2021-09-09 22:44:51 +0100

    image optimization

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,7 +5,7 @@ const Item = (props) => {
     return (
         <div className="item-container">
             <div className="img-container">
-                <img width="300" src={props.image} alt="pizza" />
+                <img width="300" height="243" src={props.image} alt="pizza" />
             </div>
             <p className="item-name">{props.name}</p>
             <div className="price-container">
